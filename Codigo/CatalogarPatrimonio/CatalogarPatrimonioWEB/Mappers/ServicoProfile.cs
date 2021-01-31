8848d612a42d6c82f9855e89a3f6c0345f7ed8c5
LilianeCosta767 <lilianecosta767@hotmail.com>
2021-01-31 11:09:25 -0300

    implementando caso de uso servico

---
+++ Codigo/CatalogarPatrimonio/CatalogarPatrimonioWEB/Mappers/ServicoProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using CatalogarPatrimonioWEB.Models;
+using Core;
+
+namespace CatalogarPatrimonioWEB.Mappers
+{
+    public class ServicoProfile : Profile
+    {
+        public ServicoProfile()
+        {
+            CreateMap<ServicoModel, Servico>().ReverseMap();
+        }
+    }
+}
---
+++ Codigo/CatalogarPatrimonio/CatalogarPatrimonioWEB/Models/ServicoModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CatalogarPatrimonioWEB.Models
+{
+    public class ServicoModel
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [Display(TipoServico = "Tipo de Serviço")]
+        [StringLength(45, MinimumLength = 5)]
+        public string tipoServico { get; set; }
+
+        [Required]
+        [Display(Predio = "Prédio")]
+        [StringLength(45, MinimumLength = 5)]
+        public string predio { get; set; }
+
+        [Required]
+        [Display(Local = "Local")]
+        [StringLength(45, MinimumLength = 5)]
+        public string local { get; set; }
+
+        [Required]
+        [Display(Horario = "Horário")]
+        [StringLength(45, MinimumLength = 5)]
+        public string horario { get; set; }
+
+        [Required]
+        [Display(Descricao = "Descrição")]
+        [StringLength(45, MinimumLength = 5)]
+        public string descricao { get; set; }
+    }
+}
