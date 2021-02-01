578d7b82ef509b80f8210898a12c40a348532252
Rafael Silveira de Andrade <rafaelsilveirarafa@gmail.com>
2021-02-01 16:27:35 -0300

    implementação do CRUD Manter Predio

---
+++ Codigo/CatalogarPatrimonio/CatalogarPatrimonioWEB/Models/PredioModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CatalogarPatrimonioWEB.Models
+{
+    public class PredioModel
+    {
+		[Display(Name = "IdPredio")]
+		[Key]
+		[Required(ErrorMessage = "Id do Predio é obrigatório")]
+		public int Id { get; set; }
+		[Required]
+		[Display(Name = "Nome")]
+		[StringLength(45, MinimumLength = 5)]
+		public string Nome { get; set; }
+		public int IdPredio { get; set; }
+		public int IdEmpresa { get; set; }
+	}
+}
