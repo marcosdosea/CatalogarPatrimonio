729d586b23093b45860e7e742863720edc7d0ad0
Daniel Santos <daniel17sants@gmail.com>
2021-02-05 11:17:09 -0300

    Ajustando os Display names

--- Codigo/CatalogarPatrimonio/CatalogarPatrimonioWEB/Models/ServicoModel.cs
+++ Codigo/CatalogarPatrimonio/CatalogarPatrimonioWEB/Models/ServicoModel.cs
@@ -11,10 +11,16 @@ namespace CatalogarPatrimonioWEB.Models
     {
         [Key]
         public int Id { get; set; }
+
+        [Display(Name = "Data da Solicitação")]
         public DateTime DataSolicitacao { get; set; }
+
+        [Display(Name = "Descrição")]
         public string Descricao { get; set; }
         public int IdSolicitante { get; set; }
         public int IdTipoServico { get; set; }
+
+        [Display(Name = "Observação")]
         public string Observacao { get; set; }
         public DateTime? DataAutorizacao { get; set; }
         public DateTime? DataConclusao { get; set; }
