b8266ad715975a04fbc9aae62f9f9830e35482b8
LilianeCosta767 <lilianecosta767@hotmail.com>
2021-02-18 22:20:58 -0300

    resolvendo isssue #193

---
+++ Codigo/CatalogarPatrimonio/CatalogarPatrimonioWEB/Mappers/TipomaterialProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using CatalogarPatrimonioWEB.Models;
+using Core;
+
+namespace CatalogarPatrimonioWEB.Mappers
+{
+    public class TipomaterialProfile : Profile
+    {
+        public TipomaterialProfile()
+        {
+            CreateMap<TipomaterialModel, Tipomaterial>().ReverseMap();
+        }
+    }
+}
---
+++ Codigo/CatalogarPatrimonio/CatalogarPatrimonioWEB/Models/TipomaterialModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CatalogarPatrimonioWEB.Models
+{
+    public class TipomaterialModel
+    {
+        [Display(Name = "Código")]
+        public int Id { get; set; }
+        public string Nome { get; set; }
+    }
+}
